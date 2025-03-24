cc157b89a54033836fd69cdd898baed067f3d46a
KarimjonovaShaxnoza <shaxnozakarimjonova100@gmail.com>
2025-03-24 15:17:26 +0500

    Styleni o'zgartirdim

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ img, name, title }) => {
   return (
     <>
-      <div className=" flex items-center w-[636px] h-[151px] bg-[#F6F6F6] mt-10 rounded-3xl mx-auto shadow-xl shadow-black/20">
+      <div className=" flex items-center w-[636px] h-[151px] bg-[#F6F6F6] mt-10 rounded-3xl shadow-xl shadow-black/20 ml-10">
         <img
           className="flex items-center justify-center w-35 h-35 bg-[#FFEFD5] rounded-full ml-8"
           src={img}
