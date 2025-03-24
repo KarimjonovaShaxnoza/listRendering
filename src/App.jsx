bbc98e48e5445297dd726302dd6839bc41aa13f3
KarimjonovaShaxnoza <shaxnozakarimjonova100@gmail.com>
2025-03-24 14:50:02 +0500

    listRendering

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "./components/Card";
+
+const users = [
+  {id: 1,img: "/male.svg",name: "Farrux Mengboyev",title: "Frontend Developer",},
+  {id: 2,img: "/nizom.svg",name: "Nizomiddin Rahimberdiyev",title: "Web Design",},
+  {id: 3,img: "/ilyos.svg", name: "Ilyosbek", title: "Web Developer" },
+  {id: 4,img: "/murod.svg",name: "Murodbek Fayzikhanov",title: "Backend Developer",},
+  {id: 5,img: "/mirfazil.svg",name: "Mirfazil Mirsharipov",title: "Frontend Developer",},
+  { id: 6, img: "/temur.svg", name: "Temur Alixanov", title: "Blogger" },
+  { id: 7, img: "/dilshod.svg", name: "dili.me", title: "Blogger" },
+  { id: 8, img: "/aslam.svg", name: "Aslamboy", title: "PUBG" },
+  { id: 9, img: "/omonxon.svg", name: "Omonxon", title: "Blogger" },
+  { id: 10, img: "/isom.svg", name: "Isomtv", title: "YouTuber" },
+];
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="pt-10 text-5xl font-bold ml-20">Users</h1>
+        <div className="grid grid-cols-2 p-10">
+        {users.map((user) => (
+          <Card key={user.id} img={user.img} name={user.name} title={user.title} />
+        ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = ({ img, name, title }) => {
+  return (
+    <>
+      <div className=" flex items-center w-[636px] h-[151px] bg-[#F6F6F6] mt-10 rounded-3xl mx-auto shadow-xl shadow-black/20">
+        <img
+          className="flex items-center justify-center w-35 h-35 bg-[#FFEFD5] rounded-full ml-8"
+          src={img}
+          alt=""
+        />
+        <div className="ml-3">
+          <h2 className="font-bold text-3xl">{name}</h2>
+          <p className="text-2xl text-gray-600">{title}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
